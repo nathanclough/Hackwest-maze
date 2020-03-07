699091947e2f745ccc32330750269306fd9a109c
Zegedam Zegeye <zegedam@gmail.com>
2020-03-07 12:31:16 -0600

    Maze converter to binary array

--- Maze_SolverZ.py
+++ Maze_SolverZ.py
@@ -1,4 +1,9 @@
-class Nodes:
-    name = "Maze Solver"
-    for i in range(0,6):
-        print(name)
+from PIL import Image
+img = Image.open('small.png')
+data = list(img.getdata())
+WIDTH, HEIGHT = img.size
+data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+for row in data:
+    print(' '.join('{:3}'.format(value) for value in row))
+
+
