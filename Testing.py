5d651d8c4a1b3b1a2782625f64cd01752bd0647d
nathanclough <nathan.clough@ttu.edu>
2020-03-07 12:33:28 -0600

    added test maze

--- Testing.py
+++ Testing.py
@@ -1,1 +1,23 @@
-print("nate")
+from Solution import Solution
+#5x5 test maze
+rows, cols = (4, 5)
+maze = [[0 for i in range(cols)] for j in range(rows)]
+maze[0][1] = 1
+maze[1][0] = 1
+maze[1][1] = 1
+maze[1][2] = 1
+maze[1][3] = 1
+maze[2][1] = 1
+maze[2][3] = 1
+maze[2][4] = 1
+maze[3][4] = 1
+sol = Solution(maze)
+sol.display()
+
+
+
+
+
+
+
+
