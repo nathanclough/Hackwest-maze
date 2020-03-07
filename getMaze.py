94e3811f551c366033b0a81adbcb06fca586dc8d
Zegedam Zegeye <zegedam@gmail.com>
2020-03-07 13:32:31 -0600

    added getMaze

---
+++ getMaze.py
@@ -0,0 +1,7 @@
+from PIL import Image
+img = Image.open('small.png')
+data = list(img.getdata())
+WIDTH, HEIGHT = img.size
+data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+for row in data:
+    print(' '.join('{:3}'.format(value) for value in row))
