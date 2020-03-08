c67150d09e6562a569be7bddd0dcf0e47bdfc6dc
Miguel <miggy2146@yahoo.com>
2020-03-08 01:26:18 -0600

    Fixed file chooser

---
+++ filepicker.py
@@ -0,0 +1,14 @@
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import shutil
+
+def opener():
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected
+    destination = "C:\Python projects\Hackwest-maze"
+    try:
+        shutil.copy(filename, destination)
+    except shutil.SameFileError:
+        pass
+    file = os.path.basename(filename)
+    return file
